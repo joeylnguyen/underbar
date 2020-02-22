0e7253effcdcf35b4392af0a7f772dc401574c30
Joey Nguyen <joeylnguyen95@gmail.com>
2020-02-22 11:52:59 +0800

    Add a for in loop for iterating over an object
    Allows the .each function to iterate over collection when the collection is an object.

--- src/underbar.js
+++ src/underbar.js
@@ -47,9 +47,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
-    if (Array.isArray(collection) === true) {
+    // Iterate when collection is an array or string
+    if (Array.isArray(collection) === true || collection.length !== undefined) {
       for (var i = 0; i < collection.length; i++) {
         iterator(collection[i], i, collection);
+      }
+    // Iterate when collection is an object
+    } else {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
       }
     }
   };
