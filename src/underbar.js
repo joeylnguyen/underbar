570e6df047342e4778c89d529a79d8b7c672143f
Joey Nguyen <joeylnguyen95@gmail.com>
2020-03-01 13:57:13 +0900

    Create _.defaults function
    Used the logic for _.extend, but added a conndition to check if the property already exists.

--- src/underbar.js
+++ src/underbar.js
@@ -339,6 +339,14 @@
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(extenderObj) { // loops through the arguments 
+      _.each(extenderObj, function(item, key) { // applies the each function and loops through each of the individual extender objects and adds the properties to the main object if it doesn't exist
+        if (obj[key] === undefined) { 
+          obj[key] = item;
+        }
+      });
+    });
+  return obj;
   };
 
 
